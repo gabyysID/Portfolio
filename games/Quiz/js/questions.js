ffd604aab347205179e5a340fbe0a33136d2d01e
gabyysID <gabriellyleitesocial@gmail.com>
2024-07-03 09:12:40 -0300

    Add files via upload

---
+++ games/Quiz/js/questions.js
@@ -0,0 +1,111 @@
+//criando um mini mini banco como dados para as perguntas.
+let questions = [
+    {
+    numb: 1,
+    question: "Longas eram as noites quando meus dias giravam em torno de você.",
+    answer: "Taylor Swift",
+    options: [
+      "Taylor Swift",
+      "Shakespeare"
+    ]
+},
+    {
+    numb: 2,
+    question: "O mundo todo é um palco.",
+    answer: "Shakespeare",
+    options: [
+      "Taylor Swift",
+      "Shakespeare"
+    ]
+},
+    {
+    numb: 3,
+    question: "Esteja o clima bem frio ou a violência desses dias difíceis.",
+    answer: "Taylor Swift",
+    options: [
+      "Taylor Swift",
+      "Shakespeare"
+    ]
+},
+    {
+    numb: 4,
+    question: "Covardes morrem muitas vezes antes de suas mortes.",
+    answer: "Shakespeare",
+    options: [
+      "Taylor Swift",
+      "Shakespeare"
+    ]
+},
+    {
+    numb: 5,
+    question: "Meu castelo desabou da noite para o dia.",
+    answer: "Taylor Swift",
+    options: [
+      "Taylor Swift",
+      "Shakespeare"
+    ]
+},
+    {
+    num: 6,
+    question: "Eu serei um sol de verão para sempre para você.",
+    answer: "Taylor Swift",
+    options: [
+        "Taylor Swift",
+        "Shakespeare"
+    ]
+},
+    {
+    num: 7,
+    question: "Esse é o êxtase do amor.",
+    answer: "Shakespeare",
+    options: [
+        "Taylor Swift",
+        "Shakespeare"
+    ]
+},
+    {
+    num: 8,
+    question: "Agora é o inverno de nosso descontentamento.",
+    answer: "Shakespeare",
+    options: [
+        "Taylor Swift",
+        "Shakespeare"
+    ]
+},
+    {
+    num: 9,
+    question: "Minha coroa, minha própria ambição e minha rainha.",
+    answer: "Shakespeare",
+    options: [
+        "Taylor Swift",
+        "Shakespeare"
+    ]
+},
+    {
+    num: 10,
+    question: "Dois finais de fogo para quatro olhos olhos azuis.",
+    answer: "Taylor Swift",
+    options: [
+        "Taylor Swift",
+        "Shakespeare"
+    ]
+},
+    {
+    num: 11,
+    question: "Os Fósforos queimam um após o outro",
+    answer: "Taylor Swift",
+    options: [
+        "Taylor Swift",
+        "Shakespeare"
+    ]
+},
+    {
+    num: 12,
+    question: "Eu não vou ficar aqui, porque os bons nunca esperam.",
+    answer: "",
+    options: [
+        "Taylor Swift",
+        "Shakespeare"
+    ]
+},
+];
